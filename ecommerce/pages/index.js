3b72845a5129e2ba9c65d41acea5f1c4696fa518
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-08 14:37:08 -0800

    pages/index.js updated with meta and link rel favicon and Head data, .babelrc created with presets: next/babel and extends in .eslintrc.json

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
 
 const index = () => {
   return (
-    <div>index</div>
+    <div>
+      <Head>
+        <title> Mandalay Star Marketplace </title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" key='title'/>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+
+      <h1> No.1 Gems & Jewellery Marketplace </h1>
+  </div>
   )
 }
 
