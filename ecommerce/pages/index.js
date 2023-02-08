7ce1e5515ef9e554b130eedb655dc36af7b7cebc
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-08 14:51:55 -0800

    deleted home.module.css default, globals.css updated with imported styling from JSMastery, index.js updated with sections for HeroBanner, Product Mapping, and Footer

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import Head from 'next/head';
 
-const index = () => {
+const Home = () => {
   return (
-    <div>
-      <Head>
-        <title> Mandalay Star Marketplace </title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" key='title'/>
-        <link rel="shortcut icon" href="/favicon.png" />
-      </Head>
+    <> 
+        <Head>
+          <title> Mandalay Star Marketplace </title>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" key='title'/>
+          <link rel="shortcut icon" href="/favicon.png" />
+        </Head>
+    {/* Hero Banner */}
+        HeroBanner
 
-      <h1> No.1 Gems & Jewellery Marketplace </h1>
-  </div>
+    {/* Product Details */}
+      <div>
+        <h2> No.1 Gems & Jewellery Marketplace </h2>
+        <p> In addition to its world-renowned rubies and jadeites, Myanmar has produced several other interesting gemstone materials. The Mogok Stone Tract has produced spinel, apatite, scapolite, moonstone, zircon, garnet, iolite and amethyst gemstones. </p>
+      </div>
+      
+      <div>
+        {['Peridot', 'Emerald', 'Sapphire', 'Tourmaline'].map((product) => product)}
+      </div>
+      
+    {/* Footer */}
+      Footer
+    </>
   )
 }
 
-export default index
+export default Home;
