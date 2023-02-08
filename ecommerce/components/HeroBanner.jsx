5dd358ef1f777be8ebf98daeffb32b01a8f1417f
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-08 15:16:12 -0800

    HeroBanner.jsx updated with banner container, img tag, Link, Description

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <div className='hero-banner-container'>
+        <div>
+            <p className='beats-solo'>
+                The August Birthstone
+            </p>
+            <h3>
+                Peridot
+            </h3>
+            <img src='' alt='peridot' className='hero-banner-image'/>
+            <div>
+                <Link href="/product/ID">
+                    <button type='button'>
+                        BUTTON
+                    </button>
+                </Link>
+                <div className='desc'>
+                    <h5>
+                        Description
+                    </h5>
+                    <p>
+                        DESCRIPTION
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
