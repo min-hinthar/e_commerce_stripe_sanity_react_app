2929c4e09c58a5e26572a0c63be1a0eeebe7e768
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-08 17:25:31 -0800

    FooterBanner.jsx updated with urlFor discount, saleTime, largeText1, largeText2, smallText, midText, product, buttonText and image props

--- ecommerce/components/FooterBanner.jsx
+++ ecommerce/components/FooterBanner.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-const FooterBanner = () => {
+import { urlFor } from '@/lib/client';
+
+const FooterBanner = ({ footerBanner: { discount, saleTime, largeText1, largeText2, smallText, midText, product, buttonText, image } }) => {
   return (
-    <div>FooterBanner</div>
+    <div className='footer-banner-container'>
+      <div className='banner-desc'>
+        <div className='left'>
+          <h3>
+            {discount}
+          </h3>
+          <h3>
+            {saleTime}
+          </h3>
+        </div>
+        <div className='right'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -4,33 +4,33 @@ import Head from 'next/head';
 import { client } from '../lib/client';
 import { Product, HeroBanner, FooterBanner } from '@/components';
 
-const Home = ({ products, bannerData }) => {
-  return (
-    <> 
-        <Head>
-          <title> Mandalay Star Gemstones </title>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" key='title'/>
-          <link rel="shortcut icon" href="/favicon.png" />
-        </Head>
-    {/* Hero Banner */}
-        <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+const Home = ({ products, bannerData }) => (
+  <> 
+    <Head>
+      <title> Mandalay Star Gemstones </title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" key='title'/>
+      <link rel="shortcut icon" href="/favicon.png" />
+    </Head>
+    <div>
+      {/* Hero Banner */}
+          <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
-    {/* Product Details */}
-      <div className='products-heading'>
-        <h2> No.1 Gems Digital Marketplace </h2>
-        <p> In addition to its world-renowned rubies and jadeites, Burma has produced several other interesting gemstone materials. The Mogok Stone Tract has produced spinel, apatite, scapolite, moonstone, zircon, garnet, iolite and amethyst gemstones. </p>
-      </div>
-      
-      <div className='products-container'>
-        {products?.map((product) => <Product key={product._id} product={product}/>)}
-      </div>
-      
-    {/* Footer */}
-      <FooterBanner />
-    </>
-  )
-};
+      {/* Product Details */}
+        <div className='products-heading'>
+          <h2> No.1 Gems Digital Marketplace </h2>
+          <p> In addition to its world-renowned rubies and jadeites, Burma is home to finest and premium gemstones. The Mogok Stone Tract has produced spinel, apatite, scapolite, moonstone, zircon, garnet, iolite and amethyst gemstones. </p>
+        </div>
+        
+        <div className='products-container'>
+          {products?.map((product) => <Product key={product._id} product={product}/>)}
+        </div>
+        
+      {/* Footer */}
+        <FooterBanner footerBanner={bannerData && bannerData[0]} />
+    </div>
+  </>
+);
 
 export const getServerSideProps = async () => {
   const query = '*[_type == "product"]';
