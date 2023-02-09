6104a4fb1b50107b1d8b4d97b9fc6b364844a36c
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-09 11:52:46 -0800

    Navbar.jsx updated with p logo Link to /, button HiShoppingBag import

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import Link from 'next/link';
+import { HiShoppingBag } from 'react-icons/hi';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/">
+          Mandalay Star Gemstones
+        </Link>
+      </p>
+      <button type='button' className='cart-icon' onClick="">
+        <HiShoppingBag />
+        <span className='cart-item-qty'>
+          1
+        </span>
+      </button>
+    </div>
   )
 }
 
