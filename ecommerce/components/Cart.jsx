3c73badb7f0bf12181744a4a03834a569262b72a
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-10 17:15:14 -0800

    Cart.jsx updated with logic to display cartItems if empty and not empty with product name, image, quantity, add, minus and total price, and delete icon

--- ecommerce/components/Cart.jsx
+++ ecommerce/components/Cart.jsx
@@ -1,7 +1,8 @@
 import React, { useRef } from 'react';
 import Link from 'next/link';
-import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
-import { TiDeleteOutline } from 'react-icons/ti';
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineShopping } from 'react-icons/ai';
+import { FaAngleDoubleRight } from 'react-icons/fa'
+import { TiDelete } from 'react-icons/ti';
 import toast from 'react-hot-toast';
 
 import { useStateContext } from '@/context/StateContext';
@@ -20,10 +21,61 @@ const Cart = () => {
           className='cart-heading'
           onClick={() => setShowCart(false)}
           >
-            <AiOutlineLeft />
+            <FaAngleDoubleRight />
             <span className='heading'>🛒 Your Cart </span>
             <span className='cart-num-items'>({totalQuantities} items)</span>
         </button>
+      {/* Shopping Cart if Empty */}
+        {cartItems.length < 1 && (
+          <div className='empty-cart'>
+            <AiOutlineShopping size={150}/>
+            <h3>
+              😅 Your Shopping Bag is Empty! 👜
+            </h3>
+            <Link href="/">
+              <button
+                type='button'
+                onClick={() => setShowCart(false)}
+                className='btn'
+              >
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
+        <div className='product-container'>
+          {cartItems.length >= 1 && cartItems.map((item) => (
+            <div className='product' key={item._id}>
+              <img src={urlFor(item?.image[0])} className='cart-product-image'/>
+              <div className='item-desc'>
+                <div className='flex top'>
+                  <h5>
+                    {item.name}
+                  </h5>
+                  <h4>
+                    ${item.price}
+                  </h4>
+                </div>
+                <div className='flex bottom'>
+                  <div>
+                    <p className='quantity-desc'>
+                        <span className='minus' onClick=""><AiOutlineMinus /></span>
+                        <span className='num'>0</span>
+                        <span className='plus' onClick=""><AiOutlinePlus /></span>
+                    </p>
+                  </div>
+                  <button 
+                    type='button'
+                    className='remove-item'
+                    onClick=''
+                  >
+                    <TiDelete />
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
