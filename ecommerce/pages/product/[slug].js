f4eac6eb895bc685d1f5809316f4171ec1f62c08
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-10 16:13:04 -0800

    Add to Cart onAdd(product, qty) updated with checkProductInCart by cartItem + quantity else product.quantity = quantity then setCartItems [...cartItems, {...product}], finally toast.success

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -12,7 +12,7 @@ const ProductDetails = ({ product, products }) => {
 
     const [ index, setIndex ] = useState(0);
 
-    const { decQty, incQty, qty } = useStateContext();
+    const { decQty, incQty, qty, onAdd } = useStateContext();
 
   return (
     <div>
@@ -69,7 +69,7 @@ const ProductDetails = ({ product, products }) => {
                     </p>
                 </div>
                 <div className='buttons'>
-                    <button type='button' className='add-to-cart' onClick=''>
+                    <button type='button' className='add-to-cart' onClick={() => onAdd(product, qty)}>
                         Add to Cart
                     </button>
                     <button type='button' className='buy-now' onClick=''>
