fd2babecd1204bc0dd8183f38854137caad39af7
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2023-02-11 17:36:39 -0800

    pages/success.js created for order confirmation page landing with thank you note, email contact and continue shopping button with Link href to homepage

---
+++ ecommerce/pages/success.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { BsBagCheckFill } from 'react-icons/bs';
+
+import { useStateContext } from '@/context/StateContext';
+
+const Success = () => {
+
+    const { setCardItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+    return (
+        <div className='success-wrapper'>
+            <div className='success'>
+                <p className='icon'>
+                    <BsBagCheckFill />
+                </p>
+                <h2>
+                    Thank you for your 💎 order!
+                </h2>
+                <p className='email-msg'>
+                    Please check your email inbox for order receipt.
+                </p>
+                <p className='description'>
+                    If you have any questions, please contact us:
+                    <a className='email' href='mailto: admin@mandalaymorningstar.com'>
+                        admin@mandalaymorningstar.com
+                    </a>
+                </p>
+                <Link href='/'>
+                    <button type='button' width='300px' className='btn'>
+                        Continue Shopping
+                    </button>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Success
